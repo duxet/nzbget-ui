ab4a12c47d4b853e7cb831b679fbc2921102f4eb
duxet <duxetlg@gmail.com>
2017-06-08 21:54:15 +0200

    Auto update list of downloads

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ extern crate serde_json;
 #[macro_use] extern crate serde_derive;
 
 use std::cell::RefCell;
+use std::collections::HashMap;
 use std::sync::mpsc::{channel, Receiver};
+use std::sync::Mutex;
 use std::thread;
 use gtk::prelude::*;
 use gtk::{AboutDialog, Builder, CellRendererText, CellRendererProgress, Label, ListStore,
-    Statusbar, Type, TreeView, TreeViewColumn, Menu, MenuItem, Widget, Window};
+    Statusbar, Type, TreeIter, TreeView, TreeViewColumn, Menu, MenuItem, Widget, Window};
 use humansize::{FileSize, file_size_opts as options};
 
 mod client;
@@ -19,6 +21,7 @@ mod client;
 #[allow(non_snake_case)]
 #[derive(Debug, Deserialize)]
 struct Group {
+    NZBID: u32,
     NZBNicename: String,
     Status: String,
     FileSizeLo: u32,
@@ -95,33 +98,26 @@ fn main() {
 
     window.show_all();
 
+    let rendered_groups: Mutex<HashMap<u32, TreeIter>> = Mutex::new(HashMap::new());
+
     let (tx, rx) = channel();
     GLOBAL.with(move |global| {
-        *global.borrow_mut() = Some((current_speed, rx))
+        *global.borrow_mut() = Some((current_speed, files_store, rendered_groups, rx))
     });
 
     thread::spawn(move || {
         loop {
-            let status = load_status();
+            let client = client::Client::new("http://localhost:6789");
 
-            tx.send(status).unwrap();
+            let groups = load_groups(&client);
+            let status = load_status(&client);
+
+            tx.send((groups, status)).unwrap();
 
             thread::sleep(std::time::Duration::from_millis(1000));
             glib::idle_add(receive);
         }
     });
-
-    let groups = load_groups();
-
-    for group in groups {
-        let file_size = format!("{}{}", group.FileSizeHi, group.FileSizeLo).parse::<i64>().unwrap();
-        let downloaded_size = format!("{}{}", group.DownloadedSizeHi, group.DownloadedSizeLo).parse::<i64>().unwrap();
-
-        let progress = downloaded_size as f64 / file_size as f64 * 100.0;
-        let human_size = file_size.file_size(options::CONVENTIONAL).unwrap();
-
-        files_store.insert_with_values(None, &[0, 1, 2, 3], &[&group.NZBNicename, &group.Status, &human_size, &progress]);
-    }
 
     window.connect_delete_event(|_, _| {
         gtk::main_quit();
@@ -131,27 +127,59 @@ fn main() {
     gtk::main();
 }
 
-fn load_groups() -> Vec<Group> {
-    let client = client::Client::new("http://localhost:6789");
+fn load_groups(client: &client::Client) -> Vec<Group> {
     let response = client.call_method("listgroups");
 
     serde_json::from_value(response.result).unwrap()
 }
 
-fn load_status() -> Status {
-    let client = client::Client::new("http://localhost:6789");
+fn load_status(client: &client::Client) -> Status {
     let response = client.call_method("status");
 
     serde_json::from_value(response.result).unwrap()
 }
 
+fn render_group(group: &Group, files_store: &ListStore) -> TreeIter {
+    let file_size = format!("{}{}", group.FileSizeHi, group.FileSizeLo).parse::<i64>().unwrap();
+    let downloaded_size = format!("{}{}", group.DownloadedSizeHi, group.DownloadedSizeLo).parse::<i64>().unwrap();
+
+    let progress = downloaded_size as f64 / file_size as f64 * 100.0;
+    let human_size = file_size.file_size(options::CONVENTIONAL).unwrap();
+
+    files_store.insert_with_values(Some(group.NZBID), &[0, 1, 2, 3], &[&group.NZBNicename, &group.Status, &human_size, &progress])
+}
+
+fn update_group(group: &Group, iter: &TreeIter, files_store: &ListStore) {
+    let file_size = format!("{}{}", group.FileSizeHi, group.FileSizeLo).parse::<i64>().unwrap();
+    let downloaded_size = format!("{}{}", group.DownloadedSizeHi, group.DownloadedSizeLo).parse::<i64>().unwrap();
+
+    let progress = downloaded_size as f64 / file_size as f64 * 100.0;
+    let human_size = file_size.file_size(options::CONVENTIONAL).unwrap();
+
+    files_store.set(iter, &[0, 1, 2, 3], &[&group.NZBNicename, &group.Status, &human_size, &progress])
+}
+
 fn receive() -> glib::Continue {
     GLOBAL.with(|global| {
-        if let Some((ref label, ref rx)) = *global.borrow() {
-            if let Ok(status) = rx.try_recv() {
+        if let Some((ref current_speed, ref files_store, ref rendered_groups, ref rx)) = *global.borrow() {
+            if let Ok(data) = rx.try_recv() {
+                let (groups, status) = data;
+                let mut rendered_groups = rendered_groups.lock().unwrap();
+
+                for group in groups {
+                    if rendered_groups.contains_key(&group.NZBID) {
+                        update_group(&group, rendered_groups.get(&group.NZBID).unwrap(), &files_store);
+                        continue;
+                    }
+
+                    let rendered_group = render_group(&group, &files_store);
+                    rendered_groups.insert(group.NZBID, rendered_group);
+                }
+
                 let mut human_speed = status.DownloadRate.file_size(options::CONVENTIONAL).unwrap();
+
                 human_speed.push_str("/s");
-                label.set_label(&human_speed);
+                current_speed.set_label(&human_speed);
             }
         }
     });
@@ -159,5 +187,5 @@ fn receive() -> glib::Continue {
 }
 
 thread_local!(
-    static GLOBAL: RefCell<Option<(Label, Receiver<Status>)>> = RefCell::new(None)
+    static GLOBAL: RefCell<Option<(Label, ListStore, Mutex<HashMap<u32, TreeIter>>, Receiver<(Vec<Group>, Status)>)>> = RefCell::new(None)
 );
